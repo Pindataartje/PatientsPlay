b11dca9872ec90afd29b8c7eb803be4fb10d791a
TeqNox <xbruintjes@gmail.com>
2025-01-08 02:14:16 +0100

    BugFix&SettingsMenuPrototype

--- Assets/Scenes/Xander/Scripting/ITEMSYSTEM/EMPItem.cs
+++ Assets/Scenes/Xander/Scripting/ITEMSYSTEM/EMPItem.cs
@@ -2,28 +2,31 @@ using UnityEngine;
 
 public class EMPItem : MonoBehaviour
 {
-    public void Use()
-    {
-        Debug.Log("EMP Grenade used! Enemy's next turn will be skipped.");
-        GameManager gameManager = FindAnyObjectByType<GameManager>();
-        if (gameManager != null)
-        {
-            gameManager.SkipEnemyTurn();
-        }
-        Destroy(gameObject); // Remove the item after use
-    }
+    [Header("EMP Settings")]
+    public AudioClip empExplosionSound; // Optional sound effect
 
     private void OnTriggerEnter(Collider other)
     {
+        // Check if the object it collided with has the "Enemy" tag
         if (other.CompareTag("Enemy"))
         {
             Debug.Log("EMP Grenade hit the enemy! Skipping their next turn.");
+
+            // Find the GameManager and trigger the SkipEnemyTurn function
             GameManager gameManager = FindAnyObjectByType<GameManager>();
             if (gameManager != null)
             {
                 gameManager.SkipEnemyTurn();
             }
-            Destroy(gameObject); // Remove the item after successful hit
+
+            // Play explosion sound if assigned
+            if (empExplosionSound != null)
+            {
+                AudioSource.PlayClipAtPoint(empExplosionSound, transform.position);
+            }
+
+            // Destroy the EMP object after collision
+            Destroy(gameObject);
         }
     }
 }
